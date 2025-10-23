292c2375ad23a0919a40380df9887040687f01ce
ingridcervello <icervema10@alumnes.ub.edu>
2025-10-23 10:27:13 +0200

    practica2

---
+++ src/Gripper_v2/src/main.cpp
@@ -0,0 +1,105 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include "MPU9250.h"
+#include <Wire.h> // Needed for I2C to read IMU
+#include <ArduinoJson.h> // Compatible amb versió 7.4.2
+#include <IMU_RoboticsUB.h>   
+
+// Device ID
+const char *deviceId = "G2_Gri";
+
+// Wi-Fi credentials
+const char *ssid = "Robotics_UB";
+const char *password = "rUBot_xx";
+
+// Vibration motor settings
+const int vibrationPin = 23; // Pin for the vibration motor
+
+// Botons
+const int PIN_S1 = 14;
+const int PIN_S2 = 27;
+int s1Status = HIGH;
+int s2Status = HIGH;
+
+// UDP settings
+IPAddress receiverESP32IP(192, 168, 1, 23); // IP of receiver ESP32
+IPAddress receiverComputerIP(192, 168, 1, 25); // IP of PC
+const int udpPort = 12345;
+WiFiUDP udp;
+
+// IMU object
+IMU imu;
+
+// Orientation data
+float Gri_roll = 0.0, Gri_pitch = 0.0, Gri_yaw = 0.0;
+
+void connectToWiFi() {
+  Serial.print("Connecting to Wi-Fi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nWi-Fi connected!");
+  Serial.println("IP Address: " + WiFi.localIP().toString());
+  Serial.print("ESP32 MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void updateOrientation() {
+  // Llegeix FIFO del DMP i actualitza càlculs interns
+  imu.ReadSensor();
+  // Obté els angles (roll, pitch, yaw) via GetRPW()
+  float* rpw = imu.GetRPW();
+  Gri_roll  = rpw[0];
+  Gri_pitch = rpw[1];
+  Gri_yaw   = rpw[2];
+  s1Status = digitalRead(PIN_S1);
+  s2Status = digitalRead(PIN_S2);
+}
+
+void sendOrientationUDP() {
+  JsonDocument doc;
+  doc["device"] = deviceId;
+  doc["roll"] = Gri_roll;
+  doc["pitch"] = Gri_pitch;
+  doc["yaw"] = Gri_yaw;
+  doc["s1"] = s1Status;
+  doc["s2"] = s2Status;
+
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer, sizeof(jsonBuffer));
+
+  // Send to ESP32 Servos
+  udp.beginPacket(receiverESP32IP, udpPort);
+  udp.write((const uint8_t*)jsonBuffer, strlen(jsonBuffer));
+  udp.endPacket();
+
+  // Send to Computer
+  udp.beginPacket(receiverComputerIP, udpPort);
+  udp.write((const uint8_t*)jsonBuffer, strlen(jsonBuffer));
+  udp.endPacket();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  delay(2000);
+
+  // Inicialitza IMU (amb DMP)
+  imu.Install();
+
+
+  connectToWiFi();
+  udp.begin(udpPort);
+  Serial.println("UDP initialized");
+
+  pinMode(PIN_S1, INPUT);
+  pinMode(PIN_S2, INPUT);
+}
+
+void loop() {
+  updateOrientation();
+  sendOrientationUDP();
+  delay(10);
+}
--- src/roboDK/Init_SurgeryRobotics_simulation.py
+++ src/roboDK/Init_SurgeryRobotics_simulation.py
@@ -137,8 +137,11 @@ def move_robot(robot, gripper, needle, text_label):
                     
         if current_Gripper_rpy:
             g_roll = Gripper_rpy.get("roll")
+            g_roll = g_roll - endo_roll
             g_pitch = Gripper_rpy.get("pitch")
+            g_pitch = g_pitch - endo_yaw
             g_yaw = Gripper_rpy.get("yaw")
+            g_yaw = g_yaw + endo_pitch
             s1 = Gripper_rpy.get("s1")
             s2 = Gripper_rpy.get("s2")
             #print(f"Gripper: {g_roll}, {g_pitch}, {g_yaw}")
